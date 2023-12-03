1b2040fda32004b172d9191b7a7a6cbddb379ddb
Rifaamllya <rifaamaliara13@gmail.com>
2023-12-03 12:08:35 +0700

    media queries

---
+++ css/js/script.js
@@ -0,0 +1,7 @@
+//toggel class active
+const navbarNav = document.querySelector ('navbar-nav');
+
+ //Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+navbarNav.classList.toggle('active')
+};
