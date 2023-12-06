13d4479ee3be27352f7f384f42326c42d58a673e
Rifaamllya <rifaamaliara13@gmail.com>
2023-12-06 20:00:48 +0700

    penambahan js sidebar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 //toggel class active
-const navbarNav = document.querySelector("navbar-nav");
+const navbarNav = document.querySelector('.navbar-nav');
 
 //Ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
 };
+
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+  if(!hamburger.contains(e.target) $$ !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
