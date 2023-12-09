96ec2d407f14f7eeaf5d5871fc900aa9e7d69973
thomaseka <thomaswastika@gmail.com>
2023-12-10 07:28:40 +0800

    replace the code location

--- js/script.js
+++ js/script.js
@@ -47,49 +47,6 @@ allStar.forEach((item, idx) => {
 });
 // end Review Star
 
-async function fetchReview() {
-  try {
-    const response = await fetch(`${BASE_API_URL}/review`);
-    const data = await response.json();
-    renderReview(data);
-  } catch (error) {
-    console.log(error);
-    throw new Error("Failed to fetch data");
-  }
-}
-
-function renderReview(data) {
-  const divisiContent = document.getElementById("review");
-  const reviewContent = data.map((review) => {
-    return `
-    <div class="testimonial-box">
-    <div class="box-top">
-      <div class="profile">
-        <div class="name-user">
-          <strong>${review.personName}</strong>
-          <span>${review.menu.menuName}</span>
-        </div>
-      </div>
-      <!--reviews------>
-      <div class="reviews">
-        <i class="fas fa-star"></i>
-        <i>${review.rating}</i>
-      </div>
-    </div>
-    <div class="client-comment">
-      <p>
-        ${review.personReview}
-      </p>
-    </div>
-  </div>
-    `;
-  });
-
-  divisiContent.innerHTML = reviewContent.join("");
-}
-
-fetchReview();
-
 //Fetch data menu from API Endpoint
 const BASE_API_URL = "https://be-2-surabaya-23-production.up.railway.app";
 
@@ -157,3 +114,47 @@ function renderMenu(data) {
 }
 
 fetchData();
+
+//fetch review
+async function fetchReview() {
+  try {
+    const response = await fetch(`${BASE_API_URL}/review`);
+    const data = await response.json();
+    renderReview(data);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch data");
+  }
+}
+
+function renderReview(data) {
+  const divisiContent = document.getElementById("review");
+  const reviewContent = data.map((review) => {
+    return `
+    <div class="testimonial-box">
+    <div class="box-top">
+      <div class="profile">
+        <div class="name-user">
+          <strong>${review.personName}</strong>
+          <span>${review.menu.menuName}</span>
+        </div>
+      </div>
+      <!--reviews------>
+      <div class="reviews">
+        <i class="fas fa-star"></i>
+        <i>${review.rating}</i>
+      </div>
+    </div>
+    <div class="client-comment">
+      <p>
+        ${review.personReview}
+      </p>
+    </div>
+  </div>
+    `;
+  });
+
+  divisiContent.innerHTML = reviewContent.join("");
+}
+
+fetchReview();
