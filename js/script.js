684ee67c7700ec2bf19fe8bd43bf1e6564271788
TrioHermanto <dcom1084@gmail.com>
2023-12-10 01:51:56 +0700

    Memperbaiki navbar and logo

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-//toggel class active
-const navbarNav = document.querySelector('.navbar-list');
-const hamburger = document.querySelector('#hamburger-menu');
+// //toggel class active
+// const navbarNav = document.querySelector('.navbar-list');
+// const hamburger = document.querySelector('#hamburger-menu');
 
-//Ketika hamburger menu di klik
-hamburger.addEventListener('click', function () {
-  navbarNav.classList.toggle('active');
+// //Ketika hamburger menu di klik
+// hamburger.addEventListener('click', function () {
+//   navbarNav.classList.toggle('active');
+// });
+
+// Navbar
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
 });
 
-document.addEventListener('click', function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active');
-  }
-});
+// document.addEventListener('click', function (e) {
+//   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+//     navbarNav.classList.remove('active');
+//   }
+// });
 
 // Review Star
 const allStar = document.querySelectorAll('.rating .star')
